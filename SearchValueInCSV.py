4446e6455f1f38f71fbe60b144d99c1de661d5ea
Norman <normcsteele@gmail.com>
2018-05-05 10:52:03 -0700

    actiivty one on cereal csv in python

---
+++ SearchValueInCSV.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+cereal_csv_path = os.path.join("cereal.csv")
+
+with open(cereal_csv_path, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    for row in csv_reader:
+        if(float(row[7]) >= 5):
+            print(row[0])
+
